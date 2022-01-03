42ddd5607ee18305b4bcbd040741c0c961d097e4
HJ <de0978@gmail.com>
2022-01-03 18:17:41 +0900

    update 조건부 내용

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -12,17 +12,22 @@ const Expense = (props) => {
     }
 
     const filteredExpense = props.items.filter(expense => {
-            return expense.date.getFullYear().toString() === filteredYear;
-        }
-    );
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpense.length > 0) {
+        expensesContent = filteredExpense.map(expense =>
+            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
+        )
+    }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-                {filteredExpense.map(expense =>
-                    <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
-                )}
+                {expensesContent}
             </Card>
         </div>
     )
