24ecb8f4f37312d012e76115d043d413f4eb2fed
HJ <de0978@gmail.com>
2022-01-03 19:34:10 +0900

    update 조건 명령문 반환을 코드 수정

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -3,6 +3,7 @@ import "./Expense.css"
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import {useState} from "react";
+import ExpensesList from "./ExpensesList";
 
 const Expense = (props) => {
     const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,23 +12,16 @@ const Expense = (props) => {
         console.log(selectedYear)
     }
 
-    const filteredExpense = props.items.filter(expense => {
+    const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
-    let expensesContent = <p>No expenses found.</p>;
-
-    if (filteredExpense.length > 0) {
-        expensesContent = filteredExpense.map(expense =>
-            <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
-        )
-    }
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter onChangeFilter={filterChangeHandler} selected={filteredYear}/>
-                {expensesContent}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     )
